d81cbd013a7ff371c6b7ca2342a34530444c87da
FairySunny <fairysunny0214@gmail.com>
2024-04-30 20:49:10 +0800

    1.20.1: fix tab button render

--- src/client/java/icu/sunny/mc/transparentwindow/mixin/client/TabButtonWidgetMixin.java
+++ src/client/java/icu/sunny/mc/transparentwindow/mixin/client/TabButtonWidgetMixin.java
@@ -1,6 +1,7 @@
 package icu.sunny.mc.transparentwindow.mixin.client;
 
-import net.minecraft.client.gui.screen.ButtonTextures;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.TabButtonWidget;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,8 +11,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @SuppressWarnings("unused")
 @Mixin(TabButtonWidget.class)
 public class TabButtonWidgetMixin {
-    @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/client/gui/screen/ButtonTextures;"))
-    private static ButtonTextures redirectStaticTexture(Identifier enabled, Identifier disabled, Identifier enabledFocused, Identifier disabledFocused) {
-        return new ButtonTextures(new Identifier("widget/button"), new Identifier("widget/button_disabled"), new Identifier("widget/button_highlighted"));
+    @Redirect(method = "renderButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawNineSlicedTexture(Lnet/minecraft/util/Identifier;IIIIIIIIIIII)V"))
+    private void redirectRenderButtonDraw(DrawContext context, Identifier texture, int x, int y, int width, int height, int leftSliceWidth, int topSliceHeight, int rightSliceWidth, int bottomSliceHeight, int centerSliceWidth, int centerSliceHeight, int u, int v) {
+        int i = v / 24 + 1;
+        if (i >= 3) i = 0;
+        context.drawNineSlicedTexture(ClickableWidget.WIDGETS_TEXTURE, x, y, width, height, 20, 4, 200, 20, 0, 46 + i * 20);
     }
 }
