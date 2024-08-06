2d8760227bde9618ce3c4f05c223a7f434f7b0b4
FairySunny <fairysunny0214@gmail.com>
2024-08-06 16:01:50 +0800

    1.0.2: allow custom panorama overlay & restore in-game menu background darkening

--- src/client/java/icu/sunny/mc/transparentwindow/mixin/client/ScreenMixin.java
+++ src/client/java/icu/sunny/mc/transparentwindow/mixin/client/ScreenMixin.java
@@ -1,5 +1,6 @@
 package icu.sunny.mc.transparentwindow.mixin.client;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,13 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @SuppressWarnings("unused")
 @Mixin(Screen.class)
 public class ScreenMixin {
-    @Inject(method = "renderPanoramaBackground", at = @At("HEAD"), cancellable = true)
-    private void injectRenderPanoramaBackground(CallbackInfo info) {
-        info.cancel();
-    }
-
     @Inject(method = "renderDarkening(Lnet/minecraft/client/gui/DrawContext;IIII)V", at = @At("HEAD"), cancellable = true)
     private void injectRenderDarkening(CallbackInfo info) {
-        info.cancel();
+        if (MinecraftClient.getInstance().world == null) {
+            info.cancel();
+        }
     }
 }
