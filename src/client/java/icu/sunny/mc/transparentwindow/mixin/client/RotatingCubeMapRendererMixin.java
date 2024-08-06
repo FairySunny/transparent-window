32f0b61d97359b5404d3b6dbcb5a7e0ebfae75e8
FairySunny <fairysunny0214@gmail.com>
2024-08-06 16:03:25 +0800

    1.0.2: allow custom panorama overlay & restore in-game menu background darkening

---
+++ src/client/java/icu/sunny/mc/transparentwindow/mixin/client/RotatingCubeMapRendererMixin.java
@@ -0,0 +1,16 @@
+package icu.sunny.mc.transparentwindow.mixin.client;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.CubeMapRenderer;
+import net.minecraft.client.gui.RotatingCubeMapRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@SuppressWarnings("unused")
+@Mixin(RotatingCubeMapRenderer.class)
+public class RotatingCubeMapRendererMixin {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/CubeMapRenderer;draw(Lnet/minecraft/client/MinecraftClient;FFF)V"))
+    private void redirectRenderCubeMap(CubeMapRenderer cubeMap, MinecraftClient client, float x, float y, float alpha) {
+    }
+}
