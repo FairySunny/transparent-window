ccc383aa47fc0dc135b49ffd92fe5e116cfdabf7
FairySunny <fairysunny0214@gmail.com>
2025-07-12 17:10:04 +0800

    1.21.4

--- src/client/java/icu/sunny/mc/transparentwindow/mixin/client/MinecraftClientMixin.java
+++ src/client/java/icu/sunny/mc/transparentwindow/mixin/client/MinecraftClientMixin.java
@@ -2,58 +2,30 @@ package icu.sunny.mc.transparentwindow.mixin.client;
 
 import com.mojang.blaze3d.platform.GlConst;
 import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
-import net.minecraft.client.gl.ShaderProgram;
-import net.minecraft.client.render.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
 @SuppressWarnings("unused")
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
     @Inject(method = "render", at = @At("HEAD"))
     private void injectRender(CallbackInfo info) {
-        MinecraftClient.getInstance().getFramebuffer().clear(MinecraftClient.IS_SYSTEM_MAC);
-    }
-
-    private static void framebufferDrawInternal(Framebuffer framebuffer, int width, int height, boolean withAlpha) {
-        RenderSystem.assertOnRenderThread();
-        GlStateManager._colorMask(true, true, true, withAlpha);
-        GlStateManager._disableDepthTest();
-        GlStateManager._depthMask(false);
-        GlStateManager._viewport(0, 0, width, height);
-        GlStateManager._disableBlend();
-
-        MinecraftClient minecraftClient = MinecraftClient.getInstance();
-        ShaderProgram shaderProgram = Objects.requireNonNull(minecraftClient.gameRenderer.blitScreenProgram, "Blit shader not loaded");
-        shaderProgram.addSampler("DiffuseSampler", framebuffer.getColorAttachment());
-        shaderProgram.bind();
-        BufferBuilder bufferBuilder = RenderSystem.renderThreadTesselator().begin(VertexFormat.DrawMode.QUADS, VertexFormats.BLIT_SCREEN);
-        bufferBuilder.vertex(0.0F, 0.0F, 0.0F);
-        bufferBuilder.vertex(1.0F, 0.0F, 0.0F);
-        bufferBuilder.vertex(1.0F, 1.0F, 0.0F);
-        bufferBuilder.vertex(0.0F, 1.0F, 0.0F);
-        BufferRenderer.draw(bufferBuilder.end());
-        shaderProgram.unbind();
-        GlStateManager._depthMask(true);
-        GlStateManager._colorMask(true, true, true, true);
+        MinecraftClient.getInstance().getFramebuffer().clear();
     }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/Framebuffer;draw(II)V"))
     private void redirectRenderFramebuffer(Framebuffer framebuffer, int width, int height, boolean tick) {
         if (tick && MinecraftClient.getInstance().world != null) {
             GlStateManager._clearColor(0, 0, 0, 1);
-            GlStateManager._clear(GlConst.GL_COLOR_BUFFER_BIT, MinecraftClient.IS_SYSTEM_MAC);
-            framebufferDrawInternal(framebuffer, width, height, false);
+            GlStateManager._clear(GlConst.GL_COLOR_BUFFER_BIT);
+            framebuffer.drawInternal(width, height);
         } else {
-            framebufferDrawInternal(framebuffer, width, height, true);
+            framebuffer.draw(width, height);
         }
     }
 }
--- src/client/java/icu/sunny/mc/transparentwindow/mixin/client/SplashOverlayMixin.java
+++ src/client/java/icu/sunny/mc/transparentwindow/mixin/client/SplashOverlayMixin.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.platform.GlConst;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.SplashOverlay;
 import net.minecraft.client.render.RenderLayer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,13 +14,20 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @SuppressWarnings("unused")
 @Mixin(SplashOverlay.class)
 public class SplashOverlayMixin {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;render(Lnet/minecraft/client/gui/DrawContext;IIF)V"))
+    private void redirectRenderScreen(Screen screen, DrawContext context, int mouseX, int mouseY, float delta) {
+        if (MinecraftClient.getInstance().world != null) {
+            screen.render(context, mouseX, mouseY, delta);
+        }
+    }
+
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;fill(Lnet/minecraft/client/render/RenderLayer;IIIII)V"))
     private void redirectRenderFill(DrawContext context, RenderLayer layer, int x1, int y1, int x2, int y2, int color) {
         if (MinecraftClient.getInstance().world != null) {
             context.fill(layer, x1, y1, x2, y2, color);
         } else {
             GlStateManager._clearColor(0, 0, 0, 0);
-            GlStateManager._clear(GlConst.GL_COLOR_BUFFER_BIT, MinecraftClient.IS_SYSTEM_MAC);
+            GlStateManager._clear(GlConst.GL_COLOR_BUFFER_BIT);
         }
     }
 
